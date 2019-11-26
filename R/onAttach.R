eae72bd8eb130adb6397b9d5f3f8c00a02982b8c
Travis CI User <travis@example.org>
2019-11-26 07:07:41 +0000

    Travis CI update 2 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 #' @import data.table
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: municipdata")
-  packageStartupMessage("Version: 2019.11.26 at 06:31")
+  packageStartupMessage("Version: 2019.11.26 at 06:51")
   packageStartupMessage("Developed by Henning \u00D8ien, Norwegian Institute of Public Health")
 }
